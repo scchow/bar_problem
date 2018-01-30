a88ea1b39b423ef04aac90c47cc39419186a6601
Scott Chow <scottchow123@gmail.com>
2018-01-30 09:23:06 -0800

    Added learning via function dependent only on epoch number:

---
+++ experiment_temp.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "MultiNightBar.hpp"
+
+int main(){
+
+    std::cout << "MultiNightBar Temp Experiment" << std::endl;
+
+    int nAgents = 100;
+    int nNights = 10;
+    int cap = 10;
+    int runFlag = 4;
+    int tau = 200;
+    bool learnTypeD = true;
+    bool impactTypeD = false;
+    double learningRate = 0.1;
+    double exploration = 0.01;
+    std::string base_path = "Results/impact_temp/";
+    int gracePeriod = 10;
+
+
+    std::vector<double> taus = {1.0, 10.0, 100.0, 300.0};
+    int numRuns = 2;
+    int numEpochs = 3000;
+
+
+    for (size_t i = 0; i < taus.size(); ++i){
+
+        for (int j = 0; j < numRuns; ++j){
+
+            std::string path = base_path + "tau_" + std::to_string(taus[i]) + "/run_" + std::to_string(j) + "/";
+
+            MultiNightBar barProblem(nAgents, nNights, cap, runFlag, taus[i], 
+                            learnTypeD, impactTypeD, 
+                            learningRate, exploration,
+                            path, gracePeriod);
+
+            for (int k = 0; k < numEpochs; ++k){
+                barProblem.simulateEpoch(k);
+            }
+
+            barProblem.logQTables();
+        }
+
+    }
+
+}
